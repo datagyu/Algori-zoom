a82ec598b625b1b0c0264a7acff1c8792a6ada36
datagyu <burnedice21@daum.net>
2026-09-17 09:26:59 +0900

    Register SWEA 5099 in visualization catalog

--- assets/catalog.js
+++ assets/catalog.js
@@ -4,6 +4,7 @@
  * 화면에 보여주지 않을 검색 키워드는 searchTerms에 넣습니다.
  */
 window.VISUALIZATIONS = [
+  {id:"swea-5099",title:"피자 굽기",platform:"SWEA",problemNo:"5099",level:"D3",href:"problems/5099/",status:"ready",searchTerms:["피자","화덕","큐","queue","선입선출","pop","append","시뮬레이션"]},
   {id:"swea-26059",title:"과일 등급 분류",platform:"SWEA",problemNo:"26059",href:"problems/26059/",status:"ready",searchTerms:["과일","등급","분류","버블 정렬","완전탐색","경계","최솟값"]},
   {"id":"swea-12712","title":"파리퇴치3","platform":"SWEA","problemNo":"12712","href":"problems/12712/","status":"ready","searchTerms":["파리 퇴치 3","델타","배열","완전탐색","십자","대각선","최댓값"]},
   {"id":"swea-9490","title":"풍선팡","platform":"SWEA","problemNo":"9490","href":"problems/9490/","status":"ready","searchTerms":["풍선","델타","배열","완전탐색","시뮬레이션","최댓값","꽃가루"]},
@@ -70,6 +71,5 @@ window.VISUALIZATIONS = [
     searchTerms: ["완전탐색", "브루트포스", "2차원 배열", "영역 합", "중첩 반복문", "최댓값"],
   },
 ];
-
 
 
